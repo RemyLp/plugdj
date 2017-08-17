159880dbd8bb24e0e6091c9d4425cdebf430a1ed
RemyLp <remiascz@gmail.com>
2017-08-17 18:32:16 +0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -112,6 +112,6 @@
     }));
 
     // Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/basicBot/source/master/basicBot.js", extend);
+    $.getScript("https://cdn.rawgit.com/RemyLp/plugdj/Reemi/reemibot.js", extend);
 
 }).call(this);
